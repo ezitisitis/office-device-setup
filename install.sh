067faaf1794b5e05f43c373e673a27f5ba827696
ezitis <git@ezitisitis.com>
2021-06-01 12:42:26 +0300

    Fix

--- install.sh
+++ install.sh
@@ -5,30 +5,21 @@ brew install --cask google-chrome
 brew install --cask google-drive
 brew install --cask microsoft-office
 
-while true; do
-  read -r -p 'Do you want add some setting changes or security? [Y/n]' security
-  case $security in
-    [Yy]* )
-      echo "Change computer name"
-      open -b com.apple.systempreferences /System/Library/PreferencePanes/SharingPref.prefPane
+read -r -p 'Do you want add some setting changes or security? [Y/n]' security
+case $security in
+  [Yy]* )
+    echo "Change computer name"
+    open -b com.apple.systempreferences /System/Library/PreferencePanes/SharingPref.prefPane
 
-      read -r "Ready for FireVault? [Y/n]" firevault
-      case $firevault in
-        '' | [Yy]* )
-          open "x-apple.systempreferences:com.apple.preference.security?FireVault"
-      esac
+    read -r "Ready for FireVault? [Y/n]" firevault
+    case $firevault in
+      '' | [Yy]* )
+        open "x-apple.systempreferences:com.apple.preference.security?FireVault"
+    esac
 
-      read -r "How about Firewall? [Y/n]" firewall
-      case $firewall in
-        '' | [Yy]* )
-          open "x-apple.systempreferences:com.apple.preference.security?Firewall"
-      break;
-      ;;
-    [Nn]* )
-      echo "Sad :("
-      break;
-      ;;
-    * )
-      echo Please answer YES or NO.
-  esac
-done
+    read -r "How about Firewall? [Y/n]" firewall
+    case $firewall in
+      '' | [Yy]* )
+        open "x-apple.systempreferences:com.apple.preference.security?Firewall"
+    esac
+esac
